953dd91e15728c70a5fd8ba3fe75c043a549f17c
Leslie Thompson <lcrthompson@gmail.com>
2020-08-18 11:30:00 -0400

    complete

--- index.js
+++ index.js
@@ -2,44 +2,43 @@
 
 /**
  * ### Challenge `processFirstItem`
- * 
+ *
  * @instructions
  * Implement a higher-order function called `processFirstItem`.
  * It takes two arguments:
  * @param stringList an array of strings.
  * @param callback function that takes a string as its argument.
  * @returns the result of invoking `callback` with the FIRST element in `stringList`.
- * 
+ *
  * Example of usage of this higher-order function:
  * Invoking `processFirstItem` passing `['foo', 'bar']` and `(str) => str + str`,
  * should return 'foofoo'.
-*/
+ */
 function processFirstItem(stringList, callback) {
-  return callback(stringList[0])
+  return callback(stringList[0]);
 }
 
 // ⭐️ Example Challenge END ⭐️
-
 
 ///// M V P ///////
 
 /* Task 1: `counterMaker`
  * Study the code for counter1 and counter2. Answer the questions below.
- * 
- * 1. What is the difference between counter1 and counter2?
- * 
- * 2. Which of the two uses a closure? How can you tell?
- * 
- * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
  *
-*/
+ * 1. What is the difference between counter1 and counter2?
+ *
+ * 2. Which of the two uses a closure? How can you tell?
+ *
+ * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better?
+ *
+ */
 
 // counter1 code
 function counterMaker() {
   let count = 0;
   return function counter() {
-   return count++;
-  }
+    return count++;
+  };
 }
 
 const counter1 = counterMaker();
@@ -51,16 +50,25 @@ function counter2() {
   return count++;
 }
 
+// console.log(counter1());
+// console.log(counter1());
+// console.log(counter1());
+// console.log(counter1());
+
+// console.log(counter2());
+// console.log(counter2());
+// console.log(counter2());
+// console.log(counter2());
 
 /* Task 2: inning() 
 
 Write a function called `inning` that returns a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
 
-function inning(/*Code Here*/){
-
-    /*Code Here*/
-
+function inning() {
+  return Math.round(Math.random() * 2);
 }
+
+console.log(inning());
 
 /* Task 3: finalScore()
 
@@ -74,13 +82,19 @@ finalScore(inning, 9) might return:
   "Away": 5,
 }
 
-*/ 
+*/
 
-function finalScore(/*code Here*/){
-
-  /*Code Here*/
-
+function finalScore(inningCB, numInnings) {
+  finScr = { Home: 0, Away: 0 };
+  // console.log(finScr.Home);
+  for (i = 0; i < numInnings; i++) {
+    finScr.Home += inningCB();
+    finScr.Away += inningCB();
+  }
+  return finScr;
 }
+
+console.log(finalScore(inning, 9));
 
 /* Task 4: 
 
@@ -102,9 +116,20 @@ and returns the score at each pont in the game, like so:
 9th inning: awayTeam - homeTeam
 Final Score: awayTeam - homeTeam */
 
-
-function scoreboard(/* CODE HERE */) {
-  /* CODE HERE */
+function scoreboard(gotScore, inningCB, numInnings) {
+  // let i = 1;
+  // let text = "";
+  let away;
+  let home;
+  // let score = gotScore(inningCB, numInnings);
+  console.log(numInnings);
+  for (i = 1; i <= numInnings; i++) {
+    // text += i + " innings: " + away + " - " + home;
+    // i++;
+    away = gotScore(inningCB, i).Away;
+    home = gotScore(inningCB, i).Home;
+    console.log(`${i} inning: ${away} - ${home}`);
+  }
 }
 
-
+scoreboard(finalScore, inning, 9);
